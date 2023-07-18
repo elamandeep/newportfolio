da7d5435c1b18ee32901993751798cbcfb0755a3
elamandeep <deep.aman6174@gmail.com>
2023-07-18 23:24:37 +0530

    minor ui update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -73,6 +73,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+    screens:{
+      "sm":"480px",
+      "md":"800px"
+    }
     },
   },
   plugins: [require("tailwindcss-animate")],
